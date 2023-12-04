993ec63031f39c1241e625646cbdacce0c08850d
Anu <irikanu@gmail.com>
2023-12-04 15:05:43 +0200

    fixed database imports, added ValidationService for login

--- database/3_import.sql
+++ database/3_import.sql
@@ -5,3 +5,19 @@ INSERT INTO storage."user" (id, role_id, email, password, status) VALUES (DEFAUL
 INSERT INTO storage."user" (id, role_id, email, password, status) VALUES (DEFAULT, 2, 'anu', '123', 'A');
 INSERT INTO storage."user" (id, role_id, email, password, status) VALUES (DEFAULT, 2, 'andrus', '123', 'D');
 INSERT INTO storage."user" (id, role_id, email, password, status) VALUES (DEFAULT, 2, 'anton', '123', 'D');
+
+INSERT INTO storage.county (id, name) VALUES (1, 'Harju');
+INSERT INTO storage.county (id, name) VALUES (2, 'Hiiu');
+INSERT INTO storage.county (id, name) VALUES (3, 'Ida-Viru');
+INSERT INTO storage.county (id, name) VALUES (4, 'Jõgeva');
+INSERT INTO storage.county (id, name) VALUES (5, 'Järva');
+INSERT INTO storage.county (id, name) VALUES (6, 'Lääne');
+INSERT INTO storage.county (id, name) VALUES (7, 'Lääne-Viru');
+INSERT INTO storage.county (id, name) VALUES (8, 'Põlva');
+INSERT INTO storage.county (id, name) VALUES (9, 'Pärnu');
+INSERT INTO storage.county (id, name) VALUES (10, 'Rapla');
+INSERT INTO storage.county (id, name) VALUES (11, 'Saare');
+INSERT INTO storage.county (id, name) VALUES (12, 'Tartu');
+INSERT INTO storage.county (id, name) VALUES (13, 'Valga');
+INSERT INTO storage.county (id, name) VALUES (14, 'Viljandi');
+INSERT INTO storage.county (id, name) VALUES (15, 'Võru');
--- src/main/java/com/example/storage/business/login/LoginController.java
+++ src/main/java/com/example/storage/business/login/LoginController.java
@@ -1,6 +1,12 @@
 package com.example.storage.business.login;
 
 import com.example.storage.business.login.dto.LoginResponse;
+import com.example.storage.infrastructure.error.ApiError;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.annotation.Resource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -13,7 +19,17 @@ public class LoginController {
     private LoginService loginService;
 
     @GetMapping("/login")
+    @Operation(summary = "Sisse logimine. Tagastab userId ja roleName.",
+            description = """
+                        Süsteemist otsitakse email ja password abil aktiivse kontoga kasutajat.
+                        kui vastet ei leita, visatakse viga errorCode'iga 111
+                    """)
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "OK"),
+            @ApiResponse(responseCode = "403", description = "Vale kasutajanimi või parool", content = @Content(schema = @Schema(implementation = ApiError.class)))
+    })
     public LoginResponse login(@RequestParam String email, @RequestParam String password) {
+
         return loginService.login(email, password);
 
     }
